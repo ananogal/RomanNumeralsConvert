0d7e64dca0d81e977d054d0a8597087dbf0122d8
Ana Nogal <ana.nogal@crowdmix.me>
2016-12-11 16:49:19 +0000

    Test 1999 ->MCMXCIX passing

--- RomanNumeralsConverterTests/RomanNumeralsConverterTests.swift
+++ RomanNumeralsConverterTests/RomanNumeralsConverterTests.swift
@@ -12,7 +12,7 @@ import XCTest
 class RomanNumeralsConverterShould: XCTestCase {
     func test_convertArabicNumberIntoRomanNumeral() {
         let converter = RomanNumeralsConverter()
-        let testableCases = [1:"I", 2:"II", 3:"III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8:"VIII", 9: "IX", 10:"X", 11: "XI", 40:"XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"]
+        let testableCases = [1:"I", 2:"II", 3:"III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8:"VIII", 9: "IX", 10:"X", 11: "XI", 40:"XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M", 1999: "MCMXCIX"]
         
         for (input, expected) in testableCases {
             XCTAssertEqual(converter.convert(input), expected)
