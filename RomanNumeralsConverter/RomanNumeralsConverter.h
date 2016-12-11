9c774994b15009c106a3e483def7b8aa15056ae4
Ana Nogal <ana.nogal@crowdmix.me>
2016-12-11 13:37:30 +0000

    Initial Commit

---
+++ RomanNumeralsConverter/RomanNumeralsConverter.h
@@ -0,0 +1,19 @@
+//
+//  RomanNumeralsConverter.h
+//  RomanNumeralsConverter
+//
+//  Created by Ana Nogal on 11/12/2016.
+//  Copyright © 2016 Codurance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RomanNumeralsConverter.
+FOUNDATION_EXPORT double RomanNumeralsConverterVersionNumber;
+
+//! Project version string for RomanNumeralsConverter.
+FOUNDATION_EXPORT const unsigned char RomanNumeralsConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RomanNumeralsConverter/PublicHeader.h>
+
+
